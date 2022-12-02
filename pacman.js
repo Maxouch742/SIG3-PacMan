2ba803f05615af03ed951217474e6a41eb9bcdf6
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2022-12-02 11:05:08 +0100

    add planification

--- pacman.js
+++ pacman.js
@@ -1,8 +1,32 @@
+// Définition des variables 
+let url = $('#server-url').val();
+url = 'http://localhost:8000';
+
+// Fonction pour faire la requête de la premiere route
+function getData() {
+    console.log("Tentative de connexion : ", url)
+    $.ajax({
+        url,
+        type: 'GET',
+        success: function(dataFromServer) {
+            console.log('Réponse :', dataFromServer);
+            updateMap(dataFromServer);
+   
+            //$('#data-get').text(
+            //    JSON.stringify(dataFromServer, undefined, 2)
+            //);
+        }
+    });
+}
+    
+
+/* ---------------------------------------------------------------------- CARTE ET OPENLAYERS ------------------------------------------------*/
 // Layers
 const osm = new ol.layer.Tile({
   source: new ol.source.OSM(),
 });
 
+// Création de la carte
 const map = new ol.Map({
   target: "map", // la cible où l'on veut charger la Map
 
@@ -13,9 +37,39 @@ const map = new ol.Map({
 
   // Vue (contrôle l'échelle, le centre, etc..)
   view: new ol.View({
-    center: ol.proj.fromLonLat([6.64, 46.77]),
-    zoom: 15,
+    center: ol.proj.fromLonLat([6.64651, 46.77167]),
+    zoom: 16,
   }),
 });
 
+function updateMap(data) {
+  /*
+  // Création d'une source vectorielle avec le GeoSJON
+  const source_road = new ol.source.Vector({
+    format: ol.format.GeoJSON(),
+    url: data
+  });
+  // Création de la couche
+  const road = new ol.layer.Vector({
+    source: source_road
+  })*/
 
+  const source_layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      features: new ol.format.GeoJSON().readFeature(data),
+    })
+  });
+
+  // Ajout à la carte
+  map.addLayer(source_layer);
+
+  var path;
+  source.once('change',function(e){
+    if (source.getState() === 'ready') {
+      path = source.getFeatures()[0];
+    }
+  });
+
+  console.log(source_layer);
+  console.log(source_road.getFeatures()[0]);
+};
--- requetegeom.py
+++ requetegeom.py
@@ -38,6 +38,8 @@ class MyServer(BaseHTTPRequestHandler) :
         # Exécution de la requête et réception des données
         self.cursor.execute("SELECT ST_AsGeoJSON(geom) FROM public.roads_quartierHopital_clean;")
         self.data = self.cursor.fetchall()
+
+        print(self.data)
         
         self.send_response(200)
         self._set_headers()
