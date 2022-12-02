73b25595fa4d4371a2b8631b1296d0e46e6bdc0b
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2022-12-02 20:00:08 +0100

    update openlayers

--- pacman.js
+++ pacman.js
@@ -2,6 +2,8 @@
 let url = $('#server-url').val();
 url = 'http://localhost:8000';
 
+getData();
+
 // Fonction pour faire la requête de la premiere route
 function getData() {
   console.log("Tentative de connexion : ", url)
@@ -20,6 +22,8 @@ function getData() {
       }
   });
 }
+
+/* https://openlayers.org/en/latest/examples/feature-move-animation.html*/
 
 /* ---------------------------------------------------------------------- CARTE ET OPENLAYERS ------------------------------------------------*/
 // Layers
@@ -46,36 +50,96 @@ const map = new ol.Map({
 function updateDataServeur(dataServer) {
   console.log('Data :', dataServer);
   road = dataServer.coordinates;
-  console.log("Coord. : ", road);
 
+  const route = new ol.format.Polyline({
+    factor: 1e6,
+  }).readGeometry(road);
 
-  /*
-  // Création d'une source vectorielle avec le GeoSJON
-  const source_road = new ol.source.Vector({
-    format: ol.format.GeoJSON(),
-    url: data
+  const routeFeature = new ol.Feature({
+    type: 'route',
+    geometry: route,
   });
-  // Création de la couche
-  const road = new ol.layer.Vector({
-    source: source_road
-  })*/
+  const startMarker = new ol.Feature({
+    type: 'icon',
+    geometry: new ol.geom.Point(route.getFirstCoordinate()),
+  });
+  const endMarker = new ol.Feature({
+    type: 'icon',
+    geometry: new ol.geom.Point(route.getLastCoordinate()),
+  });
+  const position = startMarker.getGeometry().clone();
+  const geoMarker = new ol.Feature({
+    type: 'geoMarker',
+    geometry: position,
+  });
 
-  /*const source_layer = new ol.layer.Vector({
+  const styles = {
+    'route': new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        width: 6,
+        color: [237, 212, 0, 0.8],
+      }),
+    }),
+    'icon': new ol.style.Style({
+      image: new ol.style.Icon({
+        anchor: [0.5, 1],
+        src: 'picture/icon.png',
+      }),
+    }),
+    'geoMarker': new ol.style.Style({
+      image: new ol.style.Circle({
+        radius: 7,
+        fill: new ol.style.Fill({color: 'black'}),
+        stroke: new ol.style.Stroke({
+          color: 'white',
+          width: 2,
+        }),
+      }),
+    }),
+  };
+
+  const vectorLayer = new ol.layer.Vector({
     source: new ol.source.Vector({
-      features: new ol.format.GeoJSON().readFeature(data),
-    })
+      features: [routeFeature, geoMarker, startMarker, endMarker],
+    }),
+    style: function (feature) {
+      return styles[feature.get('type')];
+    },
   });
 
-  // Ajout à la carte
-  map.addLayer(source_layer);
+  map.addLayer(vectorLayer);
 
-  var path;
-  source.once('change',function(e){
-    if (source.getState() === 'ready') {
-      path = source.getFeatures()[0];
-    }
-  });
+  console.log("ok");
 
-  console.log(source_layer);
-  console.log(source_road.getFeatures()[0]);*/
+  let distance = 0;
+  let lastTime;
+  let animating = false;
+
+  function moveFeature(event) {
+    const speed = 15;
+    const time = 50;
+    const elapsedTime = time - lastTime;
+    distance = (distance + (speed * elapsedTime) / 1e6) % 2;
+    lastTime = time;
+
+    const currentCoordinate = route.getCoordinateAt(
+      distance > 1 ? 2 - distance : distance
+    );
+    position.setCoordinates(currentCoordinate);
+    const vectorContext = ol.render.getVectorContext(event);
+    vectorContext.setStyle(styles.geoMarker);
+    vectorContext.drawGeometry(position);
+    // tell OpenLayers to continue the postrender animation
+    map.render();
+  };
+
+  function startAnimation() {
+    animating = true;
+    lastTime = Date.now();
+    vectorLayer.on('postrender', moveFeature);
+    // hide geoMarker and trigger map render through change event
+    geoMarker.setGeometry(null);
+  }
+
+  startAnimation();
 };
